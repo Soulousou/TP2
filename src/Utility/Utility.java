aace16bae577bcf262bf2b5cb037a4dab9f1ee62
Merkxic <belanger.maxime1509@gmail.com>
2022-04-29 16:57:29 -0400

    JavaDoc de Utility.java

--- src/Utility/Utility.java
+++ src/Utility/Utility.java
@@ -2,11 +2,31 @@ package Utility;
 
 import java.util.Random;
 
+/**
+ * Classe fournissant divers utilites uses dans ce projet.
+ * <p>
+ * L'idee est de combiner les fonction inclassables utilisees dans le projet.
+ */
 public class Utility {
+
+    /**
+     * Genere un nombre aleatoire compris dans les bornes specifiees.
+     * 
+     * @param seed  Randomiseur qui permetra de generer le prochain nombre
+     * @param lower Borne inferieure inclusive
+     * @param upper Borne superieure exclusive
+     * @return  Un nombre aleatoire dans l'intervale
+     */
     public static double randomInterval( Random seed, double lower, double upper){
         return seed.nextDouble()*(upper-lower) + lower;
     }
 
+    /**
+     * Methode qui transforme un entier en hexadecimal de longueur 6.
+     * 
+     * @param i Entier qui doit etre transforme
+     * @return  Un code couleur HEX
+     */
     public static String getHexColorCode(int i){
         String hexCode = Integer.toHexString(i);
         while (hexCode.length() < 6){
@@ -15,6 +35,14 @@ public class Utility {
         return hexCode;
     }
 
+    /**
+     * Permet la prise d'un choix equiprobable entre deux double.
+     * 
+     * @param seed  Randomiseur qui permetra de generer le prochain nombre
+     * @param choice1   Premiere option
+     * @param choice2   Deuxieme option
+     * @return  L'option qui a ete aleatoirement choisi.
+     */
     public static double randomChoice(Random seed, double choice1, double choice2){
         double test = seed.nextDouble();
         if(test < 0.5){
@@ -24,6 +52,14 @@ public class Utility {
         }
     }
 
+    /**
+     * Permet la prise d'un choix equiprobable entre deux int.
+     * 
+     * @param seed  Randomiseur qui permetra de generer le prochain nombre
+     * @param choice1   Premiere option
+     * @param choice2   Deuxieme option
+     * @return  L'option qui a ete aleatoirement choisi.
+     */
     public static int randomChoice(Random seed, int choice1, int choice2){
         double test = seed.nextDouble();
         if(test < 0.5){
@@ -32,14 +68,4 @@ public class Utility {
             return choice2;
         }
     }
-
-    public static Random hasard = new Random();
-    public static int nextFish() {
-        if (hasard.nextBoolean()) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
 }
