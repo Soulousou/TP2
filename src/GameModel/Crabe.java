e80cf5cd59af0ad5c6e60231edb1de624bdcc11d
Merkxic <belanger.maxime1509@gmail.com>
2022-04-29 00:08:19 -0400

    JavaDoc Crabe.java

--- src/GameModel/Crabe.java
+++ src/GameModel/Crabe.java
@@ -1,26 +1,59 @@
 /**
+ * @author: Maxime Bélanger et Sara Gair
  * Fichier : Crabe.java
  * Date: Pour le 29 avril 2022
- * Auteurs: Maxime Bélanger et Sara Gair
  */
 
 package GameModel;
 
-public class Crabe extends Fish implements Updatable{
+/**
+ * Poisson special, se deplace en allant et en retournant horizontalement sans se deplacer verticalement.
+ */
+public class Crabe extends Fish{
 
-    //periode de 0.75, 0.5 premiers forward, 0.25 dernier back
-    private final double forwardsTime = 0.5;
-    private final double periodTime = 0.75;
+    /**
+     * Temps depuis le dernier cycle avance-recule.
+     */
     private double changeInterval;
+    /**
+     * Constante de la duree passee a avancer.
+     */
+    private final double forwardsTime = 0.5;
+    /**
+     * Constante de la duree d'un cycle avance-recule.
+     */
+    private final double periodTime = 0.75;
+    /*
+    Le temps est ici implicitement divise en 2:
+    0-0.5 : Avance
+    0.5-0.75 : Recule
+    On pourrait aussi voir les constantes comme des points de
+    changement de direction.
+    */
 
+    /**
+     * Constructeur d'un crabe.
+     * <p>
+     * @see Crabe
+     * 
+     * @param game  Partie avec lequelle le crabe interagiera
+     */
     public Crabe(Game game) {
-        super(game);
-        setVX(1.3*getVX());
+        super(game);    //TODO: gerer avec un autre constructeur, pose probleme avec colorString et setURL qui doit etre redefini
+
+        this.vX = 1.3*this.vX;
         this.colorString = "XXX";
         setUrl("/Image/crabe.png");
 
     }
 
+    /**
+     * Simule le mouvement d'un crabe sur l'intervalle de temps specifie.
+     * 
+     * @see Updatable
+     * 
+     * @param deltaTime Intervalle de temps a simuler
+     */
     @Override
     public void update(double deltaTime){
         if(getAlive()){
@@ -28,16 +61,17 @@ public class Crabe extends Fish implements Updatable{
 
             this.changeInterval += deltaTime;
 
+            //Gestion de l'avance-recule
             if((this.changeInterval > this.forwardsTime && this.changeInterval - deltaTime < this.forwardsTime)){
-                setVX(-1*getVX());
+                this.vX = -1*this.vX;
             }
             if(this.changeInterval > this.periodTime){
-                setVX(-1*getVX());
+                this.vX = -1*this.vX;
                 changeInterval -= this.periodTime;
             }
 
-            setXY(getX() + getVX()*deltaTime, getY());
+            //Update position
+            this.posX = this.posX+this.vX*deltaTime;
         }
-
     }
 }
